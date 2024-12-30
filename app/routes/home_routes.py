c5f2bcee48f4300891a7a50558bdc25004102726
jubairsami <km.jubair.sami@g.bracu.ac.bd>
2024-12-30 22:23:07 +0600

    sample postman api added for lab report

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.models import get_all_published_events
+from flask import jsonify
 bp = Blueprint('home', __name__, url_prefix='')
 
 @bp.route('/')
@@ -7,4 +8,11 @@ def home():
     published_events = get_all_published_events()
     return render_template('home.html', published_events=published_events)
 
+"""
+to test postman
+@bp.route('/api/home', methods=['GET'])
+def api_events():
+    published_events = get_all_published_events()
+    return jsonify({'events': published_events})
 
+"""
